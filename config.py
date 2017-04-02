59873e620e4d00150a40c3675f95e2c68abde580
jlshix <jlshix@163.com>
2017-04-02 13:04:06 +0800

    SQLALCHEMY Operations

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 # coding: utf-8
 # 站点配置文件
+import os
+
 
 # 激活 跨站点请求伪造 保护
 CSRF_ENABLED = True
 
 # 加密秘钥
 SECRET_KEY = 'shi-yan-lou'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
